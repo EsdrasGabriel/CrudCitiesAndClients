4ba3c7edf52a2315198ff3339b850a46a4d5656d
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2024-01-11 16:57:51 -0300

    first commit

---
+++ src/test/java/com/crud/citiesandclients/CitiesandclientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.citiesandclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitiesandclientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
