047b09eb89bf01367bc092d328aec89e6910706a
EsdrasGabriel <esdrasgabriel.f00@gmail.com>
2024-01-12 16:08:18 -0300

    feat: added filter authentication

---
+++ src/main/java/com/crud/citiesandclients/infra/security/SecurityFilter.java
@@ -0,0 +1,42 @@
+package com.crud.citiesandclients.infra.security;
+
+import com.crud.citiesandclients.repositories.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    private TokenService tokenService;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = this.recoverToken(request);
+        if (token != null) {
+            String login = tokenService.validateToken(token);
+            UserDetails user = userRepository.findByLogin(login);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        String authorization = request.getHeader("Authorization");
+        if (authorization == null) return null;
+        return authorization.replace("Bearer ", "");
+    }
+}
--- src/main/java/com/crud/citiesandclients/repositories/UserRepository.java
+++ src/main/java/com/crud/citiesandclients/repositories/UserRepository.java
@@ -2,6 +2,8 @@ package com.crud.citiesandclients.repositories;
 
 import com.crud.citiesandclients.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByLogin(String login);
 }
